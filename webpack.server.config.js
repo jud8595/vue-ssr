caed7c33edeb0877c76d41c478495a07b28421f1
jdeshayes <jdeshayes.ext@galerieslafayette.com>
2019-05-22 13:43:28 +0200

    using createBundleRenderer instead of createRenderer

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -18,9 +18,15 @@ module.exports = merge(base, {
     
   output: {
     filename: 'server-bundle.js',
-	// output.libraryTarget: Configure how the library will be exposed.
-	// commonjs2: The return value of the entry point will be assigned to the module.exports
-	// cf https://stackoverflow.com/questions/38289019/require-webpack-bundle-returns-empty-object
-	libraryTarget: 'commonjs2'
-  }
+	  // output.libraryTarget: Configure how the library will be exposed.
+	  // commonjs2: The return value of the entry point will be assigned to the module.exports
+	  // cf https://stackoverflow.com/questions/38289019/require-webpack-bundle-returns-empty-object
+	  libraryTarget: 'commonjs2'
+  },
+
+  // This is the plugin that turns the entire output of the server build
+  // into a single JSON file. The default file name will be 'vue-ssr-server-bundle.json'
+  plugins: [
+    new VueSSRServerPlugin()
+  ]
 });
